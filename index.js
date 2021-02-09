65b66e847f9a42cf75aa1051a749577e867b4a8b
Ndn1618 <ndilbar1618@gmail.com>
2021-02-09 16:26:25 +0500

    Handling with erros

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const url = 'http://api.weatherstack.com/current?access_key=1954b1f03c32542fbfdb
 
 request({ url: url, json: true }, (error, response) => {
 
-  const data = response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' C.'
+  if (error) {
+    console.log('Unable to connect to weather server!')
+  } else if (response.body.error) {
+    console.log('Unable to find location. Try another')
+  } else {
+    const data = response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' C.'
 
-  console.log(data)
+    console.log(data)
+  }
 })
